e1975901d007da1e555e4784d3db20c5c847fb70
AkshitMaheshwari <akshitmaheshwari06@gmail.com>
2026-01-25 22:56:18 +0530

    Voice_mic

--- voice/mic.py
+++ voice/mic.py
@@ -3,15 +3,24 @@ import queue
 
 audio_queue = queue.Queue()
 
+_stream = None
+
 def callback(indata, frames, time, status):
     audio_queue.put(bytes(indata))
 
 def start_mic(sample_rate=16000):
-    stream = sd.RawInputStream(
+    global _stream
+    _stream = sd.RawInputStream(
         samplerate=sample_rate,
         channels=1,
         dtype="int16",
         callback=callback
     )
-    stream.start()
-    return stream
+    _stream.start()
+    return _stream
+
+def stop_mic():
+    global _stream
+    if _stream:
+        _stream.stop()
+        _stream.close()
